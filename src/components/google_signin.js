1b8abf3004fa1ffa0858f5313551278c411de62c
Jason Hirata <jhirata@integrity-apps.com>
2016-03-14 19:54:06 -1000

    added jwt creation code

--- src/components/google_signin.js
+++ src/components/google_signin.js
@@ -3,8 +3,17 @@ import React from 'react/dist/react'
 export default React.createClass({
     componentDidMount() {
 	window.onSignIn = (googleUser)=>{
-//	    console.log("Signin via google with token " + googleUser.getAuthResponse().id_token);
-	    window.sessionStorage.setItem("jwt", googleUser.getAuthResponse().id_token);
+	    //	    console.log("Signin via google with token " + googleUser.getAuthResponse().id_token);
+	    console.log("expires in " + googleUser.getAuthResponse().expires_in);
+	    //window.sessionStorage.setItem("jwt", googleUser.getAuthResponse().id_token);
+	    let sigin = Rx.Observable.fromPromise($.ajax({ url: "http://localhost:3000/authorize",
+							   method: "POST",
+							   beforeSend: xhr => {
+							       xhr.setRequestHeader("Authorization", "GoogleSignIn " + googleUser.getAuthResponse().id_token);
+							   } }));
+	    sigin.subscribe(data => {
+		window.sessionStorage.setItem("jwt", data.token);
+	    });
 	}
     },
     render() {
